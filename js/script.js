08f095610bc9ef3b5b89af1850b36418d191e68f
sergiojanko <janko.sergio1998@gmail.com>
2021-06-30 15:07:12 +0200

    input-user-data

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Input dati utente
+
+var firstName = prompt("Inserisci il tuo nome:");
+console.log("firstName : ", firstName);
+
+var lastName = prompt("Inserisci il tuo cognome:");
+console.log("lastName : ", lastName);
+
+var color = prompt("Inserisci il tuo colore preferito:");
+console.log("color : ", color);
